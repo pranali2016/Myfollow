c93bb18ad05d38f0ca54c001f79a07bd2f010d3c
pranali2016 <pranali@promactinfo.com>
2016-04-22 17:53:10 +0530

    Added files via upload

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -1,7 +1,7 @@
 <?php
 
 use Zend_Mail_Transport_Smtp;
-use Zend\Mail;
+use Zend_Mail;
 
 class AdminController extends Zend_Controller_Action
 {
@@ -16,20 +16,7 @@ class AdminController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        /*$config = array('auth' => 'login',
-                'username' => 'pranali@promactinfo.com',
-                'password' => 'lifeislov',
-                'ssl' => 'ssl',
-                'port' => 465); // Optional port number supplied
- 
-        $transport = new Zend_Mail_Transport_Smtp('smtp.gmail.com', $config);
- 
-        $mail = new Zend_Mail();
-        $mail->setBodyText('This is the text of the mail.');
-        $mail->setFrom('anita@promactinfo.com', 'Some Sender');
-        $mail->addTo('nehal@promactinfo.com', 'Some Recipient');
-        $mail->setSubject('TestSubject');
-       $mail->send($transport);*/
+        
     }
   /*  public function saveAction()
     {
@@ -43,42 +30,42 @@ class AdminController extends Zend_Controller_Action
     
     public function addAction()
     {   
-        require_once 'Zend/Mail.php';
-        //require_once 'Zend/Mail/Transport/Smtp.php';
-//   $comment = new Application_Model_Admin($this->getAllParams());
-//            $mapper  = new Application_Model_AdminMapper();
-            //$mapper->save($comment);
             $email = $this->getRequest()->getParam('email');
             $name = $this->getRequest()->getParam('name');
-            $config = array('auth' => 'login',
-                'username' => '',
-                'password' => '',
-                'ssl' => 'tls',
-            'port' => 587); // Optional port number supplied
+            
+            //echo $email." ".$name;exit;
+            $config = ['auth' => 'login',
+                'username' => 'pranali@promactinfo.com',
+                'password' => 'WhaOZRpkTY1(cPJl',
+                //'SMTPAuth ' => false,
+                //'ssl' => 'tsl',
+                //'host' => 'localhost',
+                'port' => 587
+            ]; // Optional port number supplied
  
-        $transport = new Zend_Mail_Transport_Smtp('smtp.gmail.com', $config);
+        $transport = new Zend_Mail_Transport_Smtp('webmail.promactinfo.com', $config);
+          echo "<pre>";
+        //print_r($transport);
+       
+        $mail = new Zend_Mail();
         
-        $mail = new Mail\Message();
-        $mail->setBody('This is the text of the email.');
-        $mail->setFrom('', 'Sender\'s name');
-        $mail->addTo($email, $name);
-        $mail->setSubject('TestSubject');
-
-$transport = new Mail\Transport\Sendmail();
-$transport->send($mail);
         
-//        echo "<pre>";
-//        print_r($transport);
-//        $mail = new Zend_Mail();
-//        $mail->setBodyText('This is the text of the mail.');
-//        $mail->setFrom('pranalivj9@gmail.com', 'Pranali');
-//        $mail->addTo($email, $name);
-//        $mail->setSubject('TestSubject');
-//       $mail->send($transport);
-//       print_r($mail);exit;
+        try {
+                $mail->setBodyText("hiee Redgistration link")
+                ->setBodyHtml('<a href = "http://myfollow.local/productowner">Registration link</a>')
+                ->setFrom('pranali@promactinfo.com', 'Pranali Jadhav')
+                ->addTo($email,$name)
+                ->setSubject('Registration Link')
+                ->send($transport);
+            
+        } catch (Exception $ex) {
+            echo $ex;
+        }
+        
+      $this->_helper->flashMessenger('Request is successfully sent.');
+            $this->_helper->redirector('index');
 // 
-//            $this->_helper->flashMessenger('Request is successfully sent.');
-//            $this->_helper->redirector('index');
+//            
     }
     
 }
--- application/controllers/DashboardController.php
+++ application/controllers/DashboardController.php
@@ -7,7 +7,7 @@ class DashboardController extends Zend_Controller_Action
 
     public function init()
     {
-        //provide the flashdata
+         //provide the flashdata
         $messages = $this->_helper->flashMessenger->getMessages();
         if(!empty($messages))
         $this->_helper->layout->getView()->message = $messages[0];
@@ -19,39 +19,48 @@ class DashboardController extends Zend_Controller_Action
          $mapper = new Application_Model_ProductsMapper();
             $this->session = new Zend_Session_Namespace('user_session'); //start session for product owner
             $this->view->id = $this->session->id;
-             $id = $this->session->id;
-            $result = $mapper->display($id);
-            $this->view->display = $result;
+            $id = $this->session->id;
+            
+            $result = $mapper->display($id);  // display the products.
+            $this->view->display = $result; //assign it to the view
     
     
     }
     
-    public function productsAction()    //to add new products
+    public function productsAction() //to add new products
     {
            
         $request = $this->getRequest();
-        $form    = new Application_Form_Products();
-        $this->session = new Zend_Session_Namespace('user_session');
+        $form    = new Application_Form_Products(); //create object of the form named Products
+        $this->session = new Zend_Session_Namespace('user_session'); //start session for product owner
        
         $this->view->form = $form;
-            if($this->getRequest()->isPost())
+            if($this->getRequest()->isPost()) //if the requested method id post
             {
-                if ($form->isValid($request->getPost())) {
+                if ($form->isValid($request->getPost()))
+                  { //if the inserted data is valid
                      $id = $this->session->id;
                     $intro   = $this->getRequest()->getPost('intro');
                     $comment = $this->getRequest()->getPost('comment');
-                    $upload = new Zend_File_Transfer_Adapter_Http();
+                    $upload = new Zend_File_Transfer_Adapter_Http(); //to upload file create object of file transfer adapter
                     $upload->setDestination(PUBLIC_PATH.'\uploads'); //set path where image to store in public folder
-                    $files = $upload->getFileInfo();
-                    $upload->setValidators(['Count' => ['min' => 1, 'max' => 5], 
-                                            ]);     //set max and min number fo images to upload
-                    $upload->receive();  
+                                          
+                    $files = $upload->getFileInfo(); //get the array of the form values
+                     
+                        echo "<pre>";
+                    print_r($files);
+                    $upload->receive();
+                    
+                    exit;
+                
+                      
+                    $upload->receive();  //to upload images to the uploads directory.
                      echo "<pre>";
                       $image = array();
                     foreach ($files as $file) 
                     {
                         $target_file = $file['name'];
-                        array_push($image,$target_file);
+                        array_push($image,$target_file); //push image's names to the image array
                                  
                     }
                     $image1 = $image[0];
@@ -59,23 +68,27 @@ class DashboardController extends Zend_Controller_Action
                     $image3 = $image[2];
                     $image4 = $image[3];
                     $image5 = $image[4];
-               // echo $intro." ".$comment." ".$id;
-                    $mapper = new Application_Model_ProductsMapper();
-                    $mapper->add($intro,$comment,$image1,$image2,$image3,$image4,$image5,$id);
+              
+                    $mapper = new Application_Model_ProductsMapper(); // object of the productsmapper class
+                    $pid = $mapper->add($intro,$comment,$id); //call add function to insert the data into database.
+                    foreach ($image as  $value) {
+                        if(!empty($value)){ //if image value is not empty
+                        $mapper->images($pid,$value); //add images to image table with product id
+                        }
+                    }
+                    
                     $this->_helper->flashMessenger('Product details are successfully added.'); //set flash message
                     return $this->_helper->redirector('index');
-             }
-         }
-            
-          
+                  }
+             }      
     }
     
-    public function deleteAction(){             //to delete the products
+    public function deleteAction(){   //to delete the products
         
         if($this->getRequest()->isGet()){
-            $id = $this->getRequest()->getParam('id');
+            $id = $this->getRequest()->getParam('id'); //get the product id
             $mapper = new Application_Model_ProductsMapper();
-            $mapper->delete($id);
+            $mapper->delete($id); //call delete function to delete the product with id = $id
             $this->_helper->flashMessenger('Products are successfully deleted.');   //set flash data
             return $this->_helper->redirector('index');
         }
@@ -83,24 +96,82 @@ class DashboardController extends Zend_Controller_Action
     
     public function updateAction()          // action to update the products
     {
-        $id = $this->getRequest()->getParam('id');
+        $id = $this->getRequest()->getParam('id');  //get the id to update product details
         $mapper = new Application_Model_ProductsMapper();
-        if($this->getRequest()->isPost())       //to post new updates
-        {
-            $id = $this->getRequest()->getParam('id');
-            $intro = $this->getRequest()->getParam('intro');
-            $detail = $this->getRequest()->getParam('detail');
-            $mapper->update($intro,$detail,$id);
-            $this->_helper->flashMessenger('Products are successfully updated.');
-            return $this->_helper->redirector('index');
-        }
-        if($this->getRequest()->isGet())        //get the values for update action
-        {
-            $result = $mapper->find($id);
-            $this->view->entry = $result;
+            if($this->getRequest()->isPost())       //if the request is post
+            {
+                $id = $this->getRequest()->getParam('id');
+                $intro = $this->getRequest()->getParam('intro');
+                $detail = $this->getRequest()->getParam('detail');
+
+
+                $mapper->update($intro,$detail,$id);    //call update method.
+                $this->_helper->flashMessenger('Products are successfully updated.');
+                return $this->_helper->redirector('index');
+            }
+            if($this->getRequest()->isGet())        //if the request is get
+            {
+                $result = $mapper->find($id);
+                $this->view->entry = $result;
+                $images = $mapper->image($id);
+               
+                
+                $this->view->image = $images;
+            }
+            $this->view->id = $id;
+
+    }
+    
+    public function detailsAction()
+    {
+        $this->session = new Zend_Session_Namespace('user_session'); //start session for product owner
             
+        $uid = $this->session->id;//userid
+        $intro = $this->getRequest()->getParam('intro');
+        $detail = $this->getRequest()->getParam('detail');
+        $image = array();
+        
+        if ($this->getRequest()->isPost()) {
+            $j = 0; //Variable for indexing uploaded image 
+
+                $target_path = PUBLIC_PATH."/uploads/"; //Declaring Path for uploaded images
+            for ($i = 0; $i < count($_FILES['file']['name']); $i++) {//loop to get individual element from the array
+
+                $validextensions = array("jpeg", "jpg", "png", "mp4", "3gp", "mkv", "m4v");  //Extensions which are allowed
+                $ext = explode('.', basename($_FILES['file']['name'][$i]));//explode file name from dot(.) 
+                $file_extension = end($ext); //store extensions in the variable
+
+                        $new_path = $target_path . md5(uniqid()) . "." . $ext[count($ext) - 1];//set the target path with a new name of image
+                $j = $j + 1;//increment the number of uploaded images according to the files in array       
+
+                  if (($_FILES["file"]["size"][$i] < 500000000) //Approx. 100kb files can be uploaded.
+                        && in_array($file_extension, $validextensions)) {
+                    if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $new_path)) {//if file moved to uploads folder
+                        echo $j. ').<span id="noerror">Image uploaded successfully!.</span><br/><br/>';
+                    } else {//if file was not moved.
+                        echo $j. ').<span id="error">please try again!.</span><br/><br/>';
+                    }
+                } 
+                else {//if file size and file type was incorrect.
+                    echo $j. ').<span id="error">***Invalid file Size or Type***</span><br/><br/>';
+                }
+                
+                $imagename = md5(uniqid()) . "." . $ext[count($ext) - 1];   //name of the image when it uploads to the directory
+                array_push($image, $imagename);
+            }
+            print_r($image);
+           
+            $mapper = new Application_Model_ProductsMapper(); // object of the productsmapper class
+                    $pid = $mapper->add($intro,$detail,$uid); //call add function to insert the data into database.
+                    foreach ($image as  $value) {
+                        if(!empty($value)){ //if image value is not empty
+                        $mapper->images($pid,$value); //add images to image table with product id
+                        }
+                    }
+                    
+                    $this->_helper->flashMessenger('Product details are successfully added.'); //set flash message
+                    return $this->_helper->redirector('index');
         }
-        $this->view->id = $id;
         
     }
 
